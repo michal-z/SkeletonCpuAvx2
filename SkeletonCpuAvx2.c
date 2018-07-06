2c19761a20c2d946f1f42be9d69322909fd9d9ef
Michal Ziulek <michalziulek@gmail.com>
2018-07-06 17:04:17 +0200

    Add files via upload

--- SkeletonCpuAvx2.c
+++ SkeletonCpuAvx2.c
@@ -8,6 +8,9 @@
 #include <windows.h>
 
 
+#define FORCEINLINE __forceinline
+#define ALIGN(a) __declspec(align(a))
+
 #define k_DemoName "Skeleton (CPU, AVX2)"
 #define k_DemoResolutionX 1280
 #define k_DemoResolutionY 720
@@ -20,114 +23,117 @@
 #define k_TileCountY (k_DemoResolutionY / k_TileSize)
 #define k_TileCount (k_TileCountX * k_TileCountY)
 
-typedef struct JobData
-{
-    uint8_t *displayPtr;
-} JobData;
+typedef __m256 floatpacket;
+typedef __m256i intpacket;
 
-typedef struct Demo
+typedef struct job_data
 {
-    HWND window;
-    HDC windowDc;
-    HDC memoryDc;
-    uint32_t cpuCoreCount;
-    JobData job;
-    PTP_WORK jobHandle;
-} Demo;
+    uint8_t *DisplayPtr;
+} job_data;
 
-__declspec(align(64)) static volatile uint32_t s_TileIndex[16];
+typedef struct demo
+{
+    HWND Window;
+    HDC WindowDc;
+    HDC MemoryDc;
+    uint32_t CpuCoreCount;
+    job_data Job;
+    PTP_WORK JobHandle;
+} demo;
+
+ALIGN(64) static volatile uint32_t s_TileIndex[16];
 
 static double
 GetTime(void)
 {
-    static LARGE_INTEGER startCounter;
-    static LARGE_INTEGER frequency;
-    if (startCounter.QuadPart == 0)
+    static LARGE_INTEGER StartCounter;
+    static LARGE_INTEGER Frequency;
+    if (StartCounter.QuadPart == 0)
     {
-        QueryPerformanceFrequency(&frequency);
-        QueryPerformanceCounter(&startCounter);
+        QueryPerformanceFrequency(&Frequency);
+        QueryPerformanceCounter(&StartCounter);
     }
-    LARGE_INTEGER counter;
-    QueryPerformanceCounter(&counter);
-    return (counter.QuadPart - startCounter.QuadPart) / (double)frequency.QuadPart;
+    LARGE_INTEGER Counter;
+    QueryPerformanceCounter(&Counter);
+    return (Counter.QuadPart - StartCounter.QuadPart) / (double)Frequency.QuadPart;
 }
 
 static void
-UpdateFrameTime(HWND window, double *time, float *deltaTime)
+UpdateFrameTime(HWND Window, double *Time, float *DeltaTime)
 {
-    static double lastTime = -1.0;
-    static double lastFpsTime = 0.0;
-    static uint32_t frameCount = 0;
+    static double PreviousTime = -1.0;
+    static double TextRefreshTime = 0.0;
+    static uint32_t FrameCount = 0;
 
-    if (lastTime < 0.0)
+    if (PreviousTime < 0.0)
     {
-        lastTime = GetTime();
-        lastFpsTime = lastTime;
+        PreviousTime = GetTime();
+        TextRefreshTime = PreviousTime;
     }
 
-    *time = GetTime();
-    *deltaTime = (float)(*time - lastTime);
-    lastTime = *time;
+    *Time = GetTime();
+    *DeltaTime = (float)(*Time - PreviousTime);
+    PreviousTime = *Time;
+    FrameCount++;
 
-    if ((*time - lastFpsTime) >= 1.0)
+    if ((*Time - TextRefreshTime) >= 1.0)
     {
-        double fps = frameCount / (*time - lastFpsTime);
-        double ms = (1.0 / fps) * 1000.0;
-        char text[256];
-        snprintf(text, sizeof(text), "[%.1f fps  %.3f ms] %s", fps, ms, k_DemoName);
-        SetWindowText(window, text);
-        lastFpsTime = *time;
-        frameCount = 0;
+        double FramesPerSecond = FrameCount / (*Time - TextRefreshTime);
+        double Milliseconds = (1.0 / FramesPerSecond) * 1000.0;
+        char Text[256];
+        snprintf(Text, sizeof(Text), "[%.1f fps  %.3f ms] %s", FramesPerSecond, Milliseconds, k_DemoName);
+        SetWindowText(Window, Text);
+        TextRefreshTime = *Time;
+        FrameCount = 0;
     }
-    frameCount++;
 }
 
 static LRESULT CALLBACK
-ProcessWindowMessage(HWND window, UINT message, WPARAM wparam, LPARAM lparam)
+ProcessWindowMessage(HWND Window, UINT Message, WPARAM ParamW, LPARAM ParamL)
 {
-    switch (message)
+    switch (Message)
     {
     case WM_DESTROY:
         PostQuitMessage(0);
         return 0;
     case WM_KEYDOWN:
-        if (wparam == VK_ESCAPE)
+        if (ParamW == VK_ESCAPE)
         {
             PostQuitMessage(0);
             return 0;
         }
         break;
     }
-    return DefWindowProc(window, message, wparam, lparam);
+    return DefWindowProc(Window, Message, ParamW, ParamL);
 }
 
 static void
-InitializeWindow(Demo *demo)
+InitializeWindow(demo *Demo)
 {
-    WNDCLASS winclass = {
+    WNDCLASS Winclass = {
         .lpfnWndProc = ProcessWindowMessage,
         .hInstance = GetModuleHandle(NULL),
         .hCursor = LoadCursor(NULL, IDC_ARROW),
         .lpszClassName = k_DemoName
     };
-    if (!RegisterClass(&winclass))
+    if (!RegisterClass(&Winclass))
         assert(0);
 
-    RECT rect = { 0, 0, k_DemoResolutionX, k_DemoResolutionY };
-    if (!AdjustWindowRect(&rect, WS_OVERLAPPED | WS_SYSMENU | WS_CAPTION | WS_MINIMIZEBOX, 0))
+    RECT Rect = { 0, 0, k_DemoResolutionX, k_DemoResolutionY };
+    if (!AdjustWindowRect(&Rect, WS_OVERLAPPED | WS_SYSMENU | WS_CAPTION | WS_MINIMIZEBOX, 0))
         assert(0);
 
-    demo->window = CreateWindowEx(
+    Demo->Window = CreateWindowEx(
         0, k_DemoName, k_DemoName, WS_OVERLAPPED | WS_SYSMENU | WS_CAPTION | WS_MINIMIZEBOX | WS_VISIBLE,
         CW_USEDEFAULT, CW_USEDEFAULT,
-        rect.right - rect.left, rect.bottom - rect.top,
+        Rect.right - Rect.left, Rect.bottom - Rect.top,
         NULL, NULL, NULL, 0);
-    assert(demo->window);
+    assert(Demo->Window);
 
-    demo->windowDc = GetDC(demo->window);
-    assert(demo->windowDc);
+    Demo->WindowDc = GetDC(Demo->Window);
+    assert(Demo->WindowDc);
 
-    BITMAPINFO bi = {
+    BITMAPINFO BitmapInfo = {
         .bmiHeader.biSize = sizeof(BITMAPINFOHEADER),
         .bmiHeader.biPlanes = 1,
         .bmiHeader.biBitCount = 32,
@@ -136,54 +142,55 @@ InitializeWindow(Demo *demo)
         .bmiHeader.biHeight = k_DemoResolutionY,
         .bmiHeader.biSizeImage = k_DemoResolutionX * k_DemoResolutionY
     };
-    HBITMAP hbm = CreateDIBSection(demo->windowDc, &bi, DIB_RGB_COLORS, (void **)&demo->job.displayPtr, NULL, 0);
-    assert(hbm);
+    HBITMAP BitmapHandle = CreateDIBSection(Demo->WindowDc, &BitmapInfo, DIB_RGB_COLORS, (void **)&Demo->Job.DisplayPtr,
+                                            NULL, 0);
+    assert(BitmapHandle);
 
-    demo->memoryDc = CreateCompatibleDC(demo->windowDc);
-    assert(demo->memoryDc);
+    Demo->MemoryDc = CreateCompatibleDC(Demo->WindowDc);
+    assert(Demo->MemoryDc);
 
-    SelectObject(demo->memoryDc, hbm);
+    SelectObject(Demo->MemoryDc, BitmapHandle);
 }
 
 static void CALLBACK
-DrawTiles(PTP_CALLBACK_INSTANCE instance, void *context, PTP_WORK work)
+DrawTiles(PTP_CALLBACK_INSTANCE Instance, void *Context, PTP_WORK Work)
 {
-    (void)instance;
-    (void)work;
-    JobData *data = (JobData *)context;
-    uint8_t *displayPtr = data->displayPtr;
+    (void)Instance;
+    (void)Work;
+    job_data *Data = (job_data *)Context;
+    uint8_t *DisplayPtr = Data->DisplayPtr;
 
     for (;;)
     {
-        uint32_t tileIndex = (uint32_t)_InterlockedIncrement((volatile LONG *)s_TileIndex) - 1;
-        if (tileIndex >= k_TileCount)
+        uint32_t TileIndex = (uint32_t)_InterlockedIncrement((volatile LONG *)s_TileIndex) - 1;
+        if (TileIndex >= k_TileCount)
             break;
 
-        uint32_t x0 = (tileIndex % k_TileCountX) * k_TileSize;
-        uint32_t y0 = (tileIndex / k_TileCountX) * k_TileSize;
-        uint32_t x1 = x0 + k_TileSize;
-        uint32_t y1 = y0 + k_TileSize;
+        uint32_t BeginX = (TileIndex % k_TileCountX) * k_TileSize;
+        uint32_t BeginY = (TileIndex / k_TileCountX) * k_TileSize;
+        uint32_t EndX = BeginX + k_TileSize;
+        uint32_t EndY = BeginY + k_TileSize;
 
-        for (uint32_t y = y0; y < y1; ++y)
+        for (uint32_t CurrentY = BeginY; CurrentY < EndY; ++CurrentY)
         {
-            for (uint32_t x = x0; x < x1; x += 8)
+            for (uint32_t CurrentX = BeginX; CurrentX < EndX; CurrentX += 8)
             {
-                uint32_t index = (x + y * k_DemoResolutionX) * 4;
-                _mm256_store_si256((__m256i *)&displayPtr[index], _mm256_set1_epi32(0xC0DEC0DE));
+                uint32_t Index = (CurrentX + CurrentY * k_DemoResolutionX) * 4;
+                _mm256_store_si256((__m256i *)&DisplayPtr[Index], _mm256_set1_epi32(0xC0DEC0DE));
             }
         }
     }
 }
 
 static void
-Initialize(Demo *demo)
+Initialize(demo *Demo)
 {
-    SYSTEM_INFO si;
-    GetSystemInfo(&si);
-    demo->cpuCoreCount = (uint32_t)si.dwNumberOfProcessors;
+    SYSTEM_INFO SystemInfo;
+    GetSystemInfo(&SystemInfo);
+    Demo->CpuCoreCount = (uint32_t)SystemInfo.dwNumberOfProcessors;
 
-    demo->jobHandle = CreateThreadpoolWork(DrawTiles, &demo->job, NULL);
-    assert(demo->jobHandle);
+    Demo->JobHandle = CreateThreadpoolWork(DrawTiles, &Demo->Job, NULL);
+    assert(Demo->JobHandle);
 }
 
 int
@@ -191,33 +198,33 @@ main(void)
 {
     SetProcessDPIAware();
 
-    Demo demo = { 0 };
-    InitializeWindow(&demo);
-    Initialize(&demo);
+    demo Demo = { 0 };
+    InitializeWindow(&Demo);
+    Initialize(&Demo);
 
     for (;;)
     {
-        MSG msg = { 0 };
-        if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+        MSG Message = { 0 };
+        if (PeekMessage(&Message, 0, 0, 0, PM_REMOVE))
         {
-            DispatchMessage(&msg);
-            if (msg.message == WM_QUIT)
+            DispatchMessage(&Message);
+            if (Message.message == WM_QUIT)
                 break;
         }
         else
         {
-            double time;
-            float deltaTime;
-            UpdateFrameTime(demo.window, &time, &deltaTime);
+            double Time;
+            float DeltaTime;
+            UpdateFrameTime(Demo.Window, &Time, &DeltaTime);
 
             s_TileIndex[0] = 0;
 
-            for (uint32_t i = 0; i < demo.cpuCoreCount; ++i)
-                SubmitThreadpoolWork(demo.jobHandle);
+            for (uint32_t Index = 0; Index < Demo.CpuCoreCount; ++Index)
+                SubmitThreadpoolWork(Demo.JobHandle);
 
-            WaitForThreadpoolWorkCallbacks(demo.jobHandle, FALSE);
+            WaitForThreadpoolWorkCallbacks(Demo.JobHandle, FALSE);
 
-            BitBlt(demo.windowDc, 0, 0, k_DemoResolutionX, k_DemoResolutionY, demo.memoryDc, 0, 0, SRCCOPY);
+            BitBlt(Demo.WindowDc, 0, 0, k_DemoResolutionX, k_DemoResolutionY, Demo.MemoryDc, 0, 0, SRCCOPY);
         }
     }
 
